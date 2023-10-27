3f534cdbb63fd5c60a764325166005ad5631a68e
EDUARDO <cg3018466@LAB204M16.IFSP>
2023-10-27 20:26:20 -0300

    fix: merge temp on dev

--- src/frameworks/models/userModel.ts
+++ src/frameworks/models/userModel.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {  Schema, model, Model } from "mongoose";
 import { IUserEntity } from "../../entities/iUserEntity";
 
@@ -10,21 +9,3 @@ const schema = new Schema<IUserEntity>({
 });
 
 export const UserModel: Model<IUserEntity> = model("User", schema);
-=======
-import { Schema, model, Model } from "mongoose";
-import * as mongoose from "mongoose";
-import { IUserEntity } from "../../entities/iUserEntity";
-import { Types } from "mongoose";
-
-const schema = new Schema<IUserEntity>({
-  name: { type: String },
-  lastName: { type: String },
-  documentNumber: { type: String },
-  email: { type: String, index: true, unique: true },
-});
-
-export const UserModel: Model<IUserEntity> = model<IUserEntity>(
-  "users",
-  schema
-);
->>>>>>> 69f5e7e9d2856f738ef3985763e15c8e0ab16af6
