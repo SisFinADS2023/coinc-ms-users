83fb190347a32e0b5005434c161c6fed8ac2905d
EDUARDO <cg3018466@LAB204M15.IFSP>
2023-11-30 21:49:00 -0300

    fix: removed passwords in creatre and update inputs

--- src/adapters/serializers/updateUserRequest.ts
+++ src/adapters/serializers/updateUserRequest.ts
@@ -2,7 +2,6 @@ import * as z from "zod";
 
 /**name e lastName */
 export const UpdateUserRequestSchema = z.object({
-  _id: z.string().nonempty(),
 
   name: z.string().refine(value => !/\d/.test(value), {
     message: 'Nome não pode conter caracteres numéricos',
@@ -13,23 +12,6 @@ export const UpdateUserRequestSchema = z.object({
   email: z.string().email().refine(value => value.includes('@') && value.includes('.com'), {
     message: 'O email inserido é inválido',
   }),
-  password: z.string().min(8)
-    .refine((value) => /[a-z]/.test(value), {
-      message: "A senha deve conter pelo menos uma letra minúscula",
-      path: ["password"],
-    })
-    .refine((value) => /[A-Z]/.test(value), {
-      message: "A senha deve conter pelo menos uma letra maiúscula",
-      path: ["password"],
-    })
-    .refine((value) => /\d/.test(value), {
-      message: "A senha deve conter pelo menos um dígito",
-      path: ["password"],
-    })
-    .refine((value) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(value), {
-      message: "A senha deve conter pelo menos um caractere especial",
-      path: ["password"],
-    }),
 });
 
 export type UpdateUserRequest = z.infer<typeof UpdateUserRequestSchema>;
--- src/business/usecases/input/iUpdateUserInput.ts
+++ src/business/usecases/input/iUpdateUserInput.ts
@@ -1,9 +1,7 @@
 import { ObjectId } from "bson";
 
 export interface IUpdateUserInput {
-  _id: string;
   name: string;
   lastName: string;
   email: string;
-  password: string;
 }
